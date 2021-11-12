0d754f3551e715ef764ffd30214189bb1c6b6db2
kksjonzik <kksjonzik@tktk.ee>
2021-11-12 22:55:31 +0200

    add new projects

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Are you more like a coat or a dog?");
+            string userChoice = Convert.ToString(Console.ReadLine().ToUpper());
+
+            if (userChoice == "CAT")
+            {
+                Console.WriteLine("You are a home-lover.");
+            }
+            else if (userChoice == "DOG")
+            {
+                Console.WriteLine("You are a partygoer.");
+            }
+            else 
+            {
+                Console.WriteLine($"You are a {userChoice} friend.");
+            }
+            Console.WriteLine("Have a nice day!");
+        }
+    }
+}
---
+++ GradingIF/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace GradingIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai
+            //kui kasutaja sai 'A', konsool kuvub "Suurepärane!;
+            //'B', konsool kuvab "Väga hea!";
+            //'C', konsool kuvab "Hea!";
+            //'D', konsool kuvab "Rahuldav!";
+            //'E', konsool kuvab "Kasin!";
+            //'F', konsool kuvab "Puudulik!";
+            //kui kasutaja sisestab midagi muud, siis konsool kuvab "vale väärtus"
+
+            //kasutame IF
+            
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            //char - charaster
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            if (userResult == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            else if (userResult == 'B')
+            {
+                Console.WriteLine("Väga hea!");
+            }
+            else if (userResult == 'C')
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if (userResult == 'D')
+            {
+                Console.WriteLine("Rahuldav!");
+            }
+            else if (userResult == 'E')
+            {
+                Console.WriteLine("Kasin!");
+            }
+            else if (userResult == 'F')
+            {
+                Console.WriteLine("Puudulik!");
+            }
+            else
+            {
+                Console.WriteLine($"{userResult } on vale väärtus");
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
